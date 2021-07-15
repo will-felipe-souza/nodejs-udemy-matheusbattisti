df4e7f8c86d67ffda58db4f490f93c38549cf952
William Souza <will.felipe.souza@gmail.com>
2021-07-15 18:22:59 -0300

    Escrevendo em arquivos com Node

--- 03_file_system/index.js
+++ 03_file_system/index.js
@@ -1,9 +1,17 @@
-let {readFile} = require('fs');
+let {readFile, writeFile} = require('fs');
 
 readFile('arquivo.txt', 'utf8', (error, texto) => {
   if(error) {
     throw error;
   } else {
     console.log(texto);
+  }
+});
+
+writeFile('arquivo.txt', 'Texto por write file', (error) => {
+  if(error) {
+    throw error;
+  } else {
+    console.log('Escreveu com sucesso!')
   }
 })
