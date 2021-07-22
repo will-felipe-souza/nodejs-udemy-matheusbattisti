e9bf5dde40b5a4a442db4453b054bbdcd880aaf6
William Souza <will.felipe.souza@gmail.com>
2021-07-21 23:10:18 -0300

    Testando API com Insomnia

--- 05_express/index.js
+++ 05_express/index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
   res.send('Primeira rota com Express')
 });
 
+app.get('/teste', function(req, res) {
+  res.send('Testando rota')
+});
+
 app.listen(3000, function() {
   console.log("A aplicação está funcionando na porta 3000");
 });
